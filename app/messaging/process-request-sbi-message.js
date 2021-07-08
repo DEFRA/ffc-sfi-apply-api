c17b947389f25774409ff00a518bd9e8d17df775
David Stone <davidjamesstone@gmail.com>
2021-07-08 12:35:38 +0100

    Remove request-sbi API call

--- app/messaging/process-request-sbi-message.js
+++ app/messaging/process-request-sbi-message.js
@@ -1,9 +1,9 @@
-const api = require('../api')
+// const api = require('../api')
 
 async function processRequestSBIMessage (message, receiver) {
   try {
     console.info('Received request SBI request', message.body)
-    await api.post('/request-sbi', message.body)
+    // await api.post('/request-sbi', message.body)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
