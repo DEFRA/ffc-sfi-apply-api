a59221e8e6cf4e7e925e40cbd32ace9a528fb017
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-27 13:48:37 +0100

    Add agreementNumber

--- app/messaging/process-calculate-message.js
+++ app/messaging/process-calculate-message.js
@@ -4,14 +4,13 @@ const calculatePaymentRates = require('../calculation')
 async function processCalculateMessage (message, receiver) {
   try {
     const { body, correlationId } = message
-    const { code, parcels, callerId } = body
+    const { code, parcels, callerId, agreementNumber } = body
     console.info('Received request for calculation')
     await cache.clear('calculation', correlationId)
     await cache.set('calculation', correlationId, body)
     console.info(`Request for calculation stored in cache, correlation Id: ${message.correlationId}`)
     await cache.update('calculation', message.correlationId, { ready: false })
-    const application = await cache.get('application', message.correlationId)
-    const paymentRates = await calculatePaymentRates(callerId, application.applicationId, parcels, code)
+    const paymentRates = await calculatePaymentRates(callerId, agreementNumber, parcels, code)
     await cache.update('calculation', message.correlationId, { paymentRates, ready: true })
     console.info(`Response available for calculation, correlation Id: ${correlationId}`)
     await receiver.completeMessage(message)
