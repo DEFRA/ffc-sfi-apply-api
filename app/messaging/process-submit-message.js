0a8f9400880ede553a144ebbcb34cd2577110036
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-27 13:57:27 +0100

    Add agreementNumber

--- app/messaging/process-submit-message.js
+++ app/messaging/process-submit-message.js
@@ -1,13 +1,10 @@
-const cache = require('../cache')
 const submit = require('../submit')
 
 async function processSubmitMessage (message, receiver) {
   try {
-    const { callerId, agreement } = message.body
+    const { callerId, agreement, agreementNumber } = message.body
     console.info('Received submitted agreement', message.body)
-    const application = await cache.get('application', message.correlationId)
-    console.log(application)
-    await submit(callerId, agreement.standards, application.applicationId)
+    await submit(callerId, agreement.standards, agreementNumber)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
